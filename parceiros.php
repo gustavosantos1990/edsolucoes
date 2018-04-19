acadbc7cc7a2e4c797bbfb7d1b8d4cd07cb492b6
gustavosantos1990 <gustavosisinfo2015@gmail.com>
2018-04-19 00:52:35 -0300

    Fisished partner page

--- parceiros.php
+++ parceiros.php
@@ -11,15 +11,43 @@ $currentPage = 'parceiros';
 	<hr class="my-4">
 </div>
 
+<main role="main">
 
-<div class="container">
 	
-	<div class="row">
-		
-	</div>
+
+	<div class="container marketing">
 
 
-</div>
+		<div class="row featurette">
+			<div class="col-md-7 ">
+				<h2 class="featurette-heading">Master Idiomas</h2>
+				<p class="lead">Quer aprender inglês de qualidade e conquistar aquela fluência tão desejada?!<br>
+					Seja um <strong>Master</strong>!</p>
+				</div>
+				<div class="col-md-5 ">
+					<a href="https://sites.google.com/view/masteraulasdeingles/p%C3%A1gina-inicial"  target="_blank">
+						<img class="featurette-image img-fluid mx-auto" src="images/partner-master.jpg" alt="Master Logo">
+					</a>
+				</div>
+			</div>
+
+			<hr class="featurette-divider">
+
+			<div class="row featurette">
+				<div class="col-md-7 order-md-2">
+					<h2 class="featurette-heading">MegaVale</h2>
+					<p class="lead">Desfrute de notícias e tutoriais de primera!</p>
+				</div>
+				<div class="col-md-5 order-md-1">
+					<a href="https://megavaleweb.blogspot.com.br/"  target="_blank">
+						<img class="featurette-image img-fluid mx-auto" src="images/partner-megavale.jpg" alt="Megavale Logo">
+					</a>
+				</div>
+			</div>
+
+		</div>
+
+	</main>
 
 
 <?php include('componentes/footer.php'); ?>
